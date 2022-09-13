46d8b99076f303dfa415def4aba5d731e5cf4b5e
ps160202 <pratham160202@gmail.com>
2022-09-13 11:12:32 +0530

    Dark Mode Applied on Home Page!!!

--- Treble.js
+++ Treble.js
@@ -4,48 +4,142 @@ function darkMode() {
 
 
     if(dark.checked == true) {
+        //BG
         const back = document.getElementsByClassName("container-fluid mainDiv");
-        back[0].style.backgroundColor = "rgba(0, 0, 0, 0.618)";
+        back[0].style.backgroundColor = "rgba(0, 0, 0)";
 
-        document.getElementById("navigationBar").className = "navbar navbar-expand-lg navbar-dark bg-dark rounded";
+
+        //NAVIGATION BAR
+        {
+            document.getElementById("navigationBar").className = "navbar navbar-expand-lg navbar-dark bg-dark rounded";        
+            document.getElementById("navImg").src = "Images/Treble1(White).png";
+
+            const navItems = document.getElementsByClassName("nav-item");
+            for (let i = 0; i < navItems.length; i++) {
+                navItems[i].className="nav-item nav-darkText";
+            }
+
+            document.getElementById("searchTextField").className = "form-control mr-sm-2 bg-dark";
+            document.getElementById("searchButton").className = "btn btn-outline-danger bg-dark";
+        }
         
-        document.getElementById("navImg").src = "Images/Treble1(White).png";
 
-        const navItems = document.getElementsByClassName("nav-item");
-        for (let i = 0; i < navItems.length; i++) {
-            navItems[i].className="nav-item nav-darkText";
+        //JUMBOTRON
+        {
+            document.getElementById("jumbotron").className = "jumbotron bg-dark";
+            document.getElementById("jumbotronTitle").className = "Display-1 text-light";
+            document.getElementById("darkModeSwitchLabel").style.color = "white";
+        }
+        
+        
+
+        //document.getElementById("navbarSupportedContent").style.color = "white";
+
+        
+        document.getElementById("textOnImg1").className="Display-2 textOnImgDark";
+        document.getElementById("textOnImg2").className="Display-2 textOnImgDark";
+
+        const myDivs = document.getElementsByClassName("container-fluid rounded myDiv");
+        for (let i = 0 ; i < myDivs.length ; i++) {
+            myDivs[i].className="container-fluid rounded myDiv bg-dark";
         }
 
-        document.getElementById("searchTextField").className = "form-control mr-sm-2 bg-dark";
-        document.getElementById("searchButton").className = "btn btn-outline-danger bg-dark";
-
-
-        document.getElementById("jumbotron").className = "jumbotron bg-dark";
-        document.getElementById("jumbotronTitle").className = "Display-1 text-light";
+        document.getElementById("musicText").className="Display-4 text-light";
+        document.getElementById("aboutText").className="container Display-5 text-light";
         
 
-        document.getElementById("navbarSupportedContent").style.color = "white";
+        //CARDS
+        {
+            const cardsBG = document.getElementsByClassName("card myCard");
+            for (let i = 0 ; i < cardsBG.length ; i++) {
+                cardsBG[i].className="card myCard bg-dark";
+            }
 
-        document.getElementById("textOnImgID").className = "Display-2 textOnImgDark";
+            const cardsText = document.getElementsByClassName("card-text");
+            for (let i = 0 ; i < cardsText.length ; i++) {
+                cardsText[i].className="card-text text-light";
+            }
+        }
 
-        
+
+        //STATISTICS
+        {
+            const statsText = document.getElementsByClassName("display-3");
+            for (let i = 0 ; i < statsText.length ; i++) {
+                statsText[i].className="display-3 text-light";
+            }
+
+            document.getElementById("statsRow").style.backgroundColor = "rgba(50, 50, 50, 0.73)";
+        }
+
+        const hr = document.getElementsByClassName("myHR");
+        hr[0].className="myHRDark";
         
     } else {
+        //BG
         const back = document.getElementsByClassName("container-fluid mainDiv");
         back[0].style.backgroundColor = "rgba(76, 74, 74, 0.278)";
 
-        document.getElementById("navigationBar").className = "navbar navbar-expand-lg navbar-light bg-light rounded";
-        document.getElementById("navImg").src = "Images/Treble1.png";
-        const navItems = document.getElementsByClassName("nav-item");
-        for (let i = 0; i < navItems.length; i++) {
-            //navItems[i].style.color = "white";
-            navItems[i].className="nav-item";
-        }
-        document.getElementById("searchTextField").className = ("form-control mr-sm-2");
-        document.getElementById("searchButton").className = ("btn btn-outline-danger");
 
-        document.getElementById("jumbotron").className = "jumbotron bg-light";
-        document.getElementById("jumbotronTitle").className = "Display-1 text-dark";
+        //NAVIGATION BAR
+        {
+            document.getElementById("navigationBar").className = "navbar navbar-expand-lg navbar-light bg-light rounded";
+            document.getElementById("navImg").src = "Images/Treble1.png";
+            const navItems = document.getElementsByClassName("nav-item");
+            for (let i = 0; i < navItems.length; i++) {
+                //navItems[i].style.color = "white";
+                navItems[i].className="nav-item";
+            }
+            document.getElementById("searchTextField").className = ("form-control mr-sm-2");
+            document.getElementById("searchButton").className = ("btn btn-outline-danger");
+        }
+        
+
+        //JUMNOTRON
+        {
+            document.getElementById("jumbotron").className = "jumbotron bg-light";
+            document.getElementById("jumbotronTitle").className = "Display-1 text-dark";
+            document.getElementById("darkModeSwitchLabel").style.color = "black";
+        }
+        
+        document.getElementById("textOnImg1").className="Display-2 textOnImg";
+        document.getElementById("textOnImg2").className="Display-2 textOnImg";
+
+
+        const myDivs = document.getElementsByClassName("container-fluid rounded myDiv");
+        for (let i = 0; i < myDivs.length; i++) {
+            myDivs[i].className="container-fluid rounded myDiv";
+        }
+
+        document.getElementById("musicText").className="Display-4";
+        document.getElementById("aboutText").className="container Display-5";
+
+
+        //CARDS
+        {
+            const cardsBG = document.getElementsByClassName("card myCard");
+            for (let i = 0 ; i < cardsBG.length ; i++) {
+                cardsBG[i].className="card myCard";
+            }
+
+            const cardsText = document.getElementsByClassName("card-text");
+            for (let i = 0 ; i < cardsText.length ; i++) {
+                cardsText[i].className="card-text";
+            }
+        }
+
+        //STATISTICS
+        {
+            const statsText = document.getElementsByClassName("display-3");
+            for (let i = 0 ; i < statsText.length ; i++) {
+                statsText[i].className="display-3";
+            }
+
+            document.getElementById("statsRow").style.backgroundColor = "rgba(135, 135, 135, 0.73)";
+        }
+
+        const hr = document.getElementsByClassName("myHR");
+        hr[0].className="myHR";
     }
 
 }
